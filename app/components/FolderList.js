a5299a525061a5280799e59714b50767bab8d1c2
Yash <78804783+yashd-dev@users.noreply.github.com>
2023-12-09 22:06:33 +0530

    Updated UI

--- app/components/FolderList.js
+++ app/components/FolderList.js
@@ -1,37 +1,68 @@
 "use client";
-import Link from 'next/link';
-import { useState, useEffect } from 'react';
-import client from '../utils/sanity';
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import client from "../utils/sanity";
 
 export default function FolderList() {
-  const [folders, setFolders] = useState([]);
+	const [folders, setFolders] = useState([]);
 
-  useEffect(() => {
-    const fetchFolders = async () => {
-      const result = await client.fetch('*[_type == "folder"]{_id, title, description, image, links[]->{_id, title, url}}');
-      setFolders(result);
-    };
-    console.log(folders);
+	useEffect(() => {
+		const fetchFolders = async () => {
+			const result = await client.fetch(
+				'*[_type == "folder"]{_id, title, description, image, links[]->{_id, title, url}}'
+			);
+			setFolders(result);
+		};
 
-    fetchFolders();
-  }, []);
+		fetchFolders();
+	}, []);
+	let arr = 0;
+	return (
+		<div className="min-w-full flex flex-col justify-center items-center">
+			<h1
+				className="  font-bold text-4xl md:text-6xl underline decoration-wavy md:decoration-solid md:decoration-8 decoration-black animate-fade-right animate-once animate-ease-in-out animate-normal"
+				style={{ animationDelay: `${0.3}s` }} // Add staggered delay
+			>
+				Our Links!
+			</h1>
 
-  return (
-    <div>
-      <h1>Folders</h1>
-      <ul>
-        {folders.map(folder => (
-          <li key={folder._id}>
-            <Link href={`/${encodeURIComponent(folder.title)}`}>
-         
-                <h3>{folder.title}</h3>
-                <p>{folder.description}</p>
-                {folder.image && <img src={folder.image.url} alt={folder.title} />}
-      
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+			<ul>
+				{folders.map((folder, index) => {
+					return (
+						<Link
+							href={`/${encodeURIComponent(folder.title)}`}
+							className={`card group animate-fade-right animate-once animate-ease-in-out animate-normal `}
+							style={{ animationDelay: `${index + 1 * 0.3}s` }} // Add staggered delay
+						>
+							<li
+								key={folder._id}
+								className="drop-shadow-[8px_8px_0px_#111] group-hover:drop-shadow-[16px_16px_0px_#111] bg-naitechh-green md:bg-naitechh-purple p-3 my-8 md:gap-4 rounded-xl border-4 border-black text-black min-w-[65vw] lg:min-w-[40rem] flex flex-col md:flex-row justify-center items-center transition-all duration-300 ease-in-out"
+							>
+								{folder.image && (
+									<img
+										src={folder.image.url}
+										alt={folder.title}
+										className="bg-naitechh-surface p-3"
+									/>
+								)}
+
+								<img
+									src="https://images.unsplash.com/photo-1683009427479-c7e36bbb7bca?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"
+									className="max-w-[10rem] md:pr-3 rounded-xl md:rounded-none md:border-r-2 border-black drop-shadow-xl"
+								/>
+								<span className="w-full min-h-full text-lg md:text-2xl font-bold flex flex-col justify-center">
+									<h3 className="md:border-b-2 border-black mb-2 text-center p-2">
+										{folder.title}
+									</h3>
+									<p className="text-center p-2">
+										{folder.description}
+									</p>
+								</span>
+							</li>
+						</Link>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				// https://flatuicolors.com/palette/se
+				naitechh: {
+					base: "#809bce",
+					surface: "#f3f4ef",
+					accent: "#c8befd",
+					purple: "#d0adfc",
+					violet: "#ceadfc",
+					green: "#23a093",
+					red: "#e2442f",
+					orange: "#ed7f07",
+					green: "#239f94",
+				},
+			},
+		},
+	},
+	plugins: [require("tailwindcss-animated")],
+};
